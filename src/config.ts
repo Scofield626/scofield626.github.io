b92d73f3724d418ea07ba8aa7e928ed4f693a035
Scofield626 <765203006@qq.com>
2023-06-14 20:01:24 +0100

    update

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Zhengqing (Scofield) Liu'
+export const SITE_TITLE = 'Scofield Z.Q. Liu'
 export const SITE_DESCRIPTION = "Scofield's personal website!"
-export const CV_PATH = '/CV_Scofield_10_22.pdf'
+export const CV_PATH = '/CV_Scofield_06_23.pdf'
+export const BLOG_PATH = "https://scofield-liew.bearblog.dev/"
